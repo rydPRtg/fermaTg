eacaf11e4009c0a4f6f37d1a2b41ef1317a1ec0d
rydPrtg <rydpr24@gmail.com>
2024-09-09 09:24:08 +0300

    monet

--- game/script.js
+++ game/script.js
@@ -1,6 +1,13 @@
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-  square.addEventListener('click', () => {
-    square.classList.toggle('clicked');
+document.addEventListener("DOMContentLoaded", function() {
+  let coins = 0;
+  const coinCounter = document.getElementById('coin-counter');
+  const squares = document.querySelectorAll('.square');
+
+  squares.forEach(square => {
+    square.addEventListener('click', () => {
+      square.classList.toggle('clicked');
+      coins += 1;  // Увеличиваем количество монет
+      coinCounter.textContent = `Монеты: ${coins}`;  // Обновляем счетчик
+    });
   });
 });
