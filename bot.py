168ce017d753336f11eb7aa5ac3210c40034fadd
rydPrtg <rydpr24@gmail.com>
2024-09-17 23:47:56 +0300

    без сервера

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from aiogram.types import InputFile
+from aiogram.types import InputFile, InlineKeyboardMarkup, InlineKeyboardButton
 
-API_TOKEN = '7308654023:AAF1hdW4f6VyeZnkn0kX946kIY6k7_xz9ZU'
+API_TOKEN = '7308654023:AAF1hdW4f6VyeZnkn0kX946kIY6k7_xz9ZU'  # Замените на ваш токен
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
@@ -12,7 +12,6 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
 # Команда /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -20,20 +19,16 @@ async def send_welcome(message: types.Message):
     photo_path = 'images/welcome.jpg'  # Убедитесь, что путь правильный и файл существует
     photo = InputFile(photo_path)
 
-    # Отправляем картинку с сообщением
+    # Создаем клавиатуру с кнопками
+    markup = InlineKeyboardMarkup()
+    channel_button = InlineKeyboardButton("Перейти в канал", url="https://t.me/+ZFruH_uujRk1NmFi")
+    game_button = InlineKeyboardButton("Запустить игру", web_app=types.WebAppInfo(url="https://rydprtg.github.io/fermaTg/game/index.html"))
+    markup.add(channel_button, game_button)
+
+    # Отправляем картинку с сообщением и кнопками
     await bot.send_photo(chat_id=message.chat.id, photo=photo,
                          caption="Привет! Добро пожаловать на ферму. Нажми на кнопку ниже, чтобы начать игру.",
-                         reply_markup=game_button())
-
-
-# Кнопка для запуска игры через WebApp
-def game_button():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    web_app = types.WebAppInfo(url="https://rydprtg.github.io/fermaTg/game/index.html")
-    game_button = types.KeyboardButton(text="Запустить игру", web_app=web_app)
-    markup.add(game_button)
-    return markup
-
+                         reply_markup=markup)
 
 # Обработка нажатий на другие кнопки (например, "Обработать", "Собрать урожай" и т.д.)
 @dp.message_handler()
@@ -50,7 +45,6 @@ async def process_buttons(message: types.Message):
         await message.reply("Вы выбрали: Задание!")
     elif message.text == "Рейтинг":
         await message.reply("Вы выбрали: Рейтинг!")
-
 
 # Запуск бота
 if __name__ == '__main__':
